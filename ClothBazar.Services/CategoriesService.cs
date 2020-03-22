e45207bac246ae7bbf3ad9b856c5a5f0f941216c
Naim0307 <mdnaimul15902@gmail.com>
2020-03-22 17:09:50 +0600

    Creat New Repositary,Contoller and View

---
+++ ClothBazar.Services/CategoriesService.cs
@@ -0,0 +1,38 @@
+﻿using ClothBazar.DataBase;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Services
+{
+    public class CategoriesService
+    {
+        public Category GetCategory(int Id)
+        {
+            using (var dbcontext = new CBContext())
+            {
+                return dbcontext.Categories.Find(Id);
+
+            }
+        }
+        public List<Category> GetCategories()
+        {
+            using (var dbcontext = new CBContext())
+            {
+               return dbcontext.Categories.ToList();
+               
+            }
+        }
+        public void SaveCategory(Category category)
+        {
+            using(var dbcontext= new CBContext())
+            {
+                dbcontext.Categories.Add(category);
+                dbcontext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ClothBazar.Web/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using ClothBazar.Entities;
+using ClothBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class CategoryController : Controller
+    {
+        CategoriesService CategoriesService = new CategoriesService();
+        public ActionResult Index()
+        {
+            var categories = CategoriesService.GetCategories();
+            return View(categories);
+        }
+        // GET: Category
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Category category)
+        {
+            CategoriesService.SaveCategory(category);
+            return View();
+        }
+        [HttpGet]
+        public ActionResult Edit(int Id)
+        {
+            var category = CategoriesService.GetCategory(Id);
+            return View(category);
+        }
+    }
+}
