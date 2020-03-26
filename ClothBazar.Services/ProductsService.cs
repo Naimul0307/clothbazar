d8cacdcbc96623f90611b4b3b4b64e3f62fc9b21
Naim0307 <mdnaimul15902@gmail.com>
2020-03-26 12:33:08 +0600

    creat product,user jaquery in seacrh button to search product

---
+++ ClothBazar.Services/ProductsService.cs
@@ -0,0 +1,58 @@
+﻿using ClothBazar.DataBase;
+using ClothBazar.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClothBazar.Services
+{
+    public class ProductsService
+    {
+        public Product GetProduct(int Id)
+        {
+            using (var dbcontext = new CBContext())
+            {
+                return dbcontext.Products.Find(Id);
+
+            }
+        }
+        public List<Product> GetProducts()
+        {
+            using (var dbcontext = new CBContext())
+            {
+                return dbcontext.Products.ToList();
+
+            }
+        }
+        public void SaveProduct(Product product)
+        {
+            using (var dbcontext = new CBContext())
+            {
+                dbcontext.Products.Add(product);
+                dbcontext.SaveChanges();
+            }
+        }
+
+        public void UpdateProduct(Product product)
+        {
+            using (var dbcontext = new CBContext())
+            {
+                dbcontext.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                dbcontext.SaveChanges();
+            }
+        }
+        public void DeleteProduct(int Id)
+        {
+            using (var dbcontext = new CBContext())
+            {
+                //dbcontext.Entry(category).State = System.Data.Entity.EntityState.Deleted;
+
+                var product = dbcontext.Products.Find(Id);
+                dbcontext.Products.Remove(product);
+                dbcontext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ ClothBazar.Web/Controllers/ProductController.cs
@@ -0,0 +1,44 @@
+﻿using ClothBazar.Entities;
+using ClothBazar.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClothBazar.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductsService productsService = new ProductsService();
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductTable(string search)
+        {
+             var products = productsService.GetProducts();
+            if (!string.IsNullOrEmpty(search))
+            {
+                products = products.Where(p => p.Name == search).ToList();
+            }
+
+            return PartialView(products);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return PartialView();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Product product)
+        {
+            productsService.SaveProduct(product);
+            return RedirectToAction("ProductTable");
+        }
+    }
+}
